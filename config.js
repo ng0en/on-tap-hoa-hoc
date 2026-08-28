ebbeb94fc7014a966403ca19ca4b75131a702e80
ng0en <106650654+ng0en@users.noreply.github.com>
2026-08-28 09:17:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Dán URL Web App của Google Apps Script vào đây sau khi deploy (xem HUONG_DAN_DEPLOY.md).
 // Ví dụ: "https://script.google.com/macros/s/AKfycb.../exec"
-window.API_URL = "";
+window.API_URL = "https://script.google.com/macros/s/AKfycbybx7_96VFVJ2uVRSuUC1AaEEVjUPqCqDld4WWnyVVSU_8jblKVYk9ybHI3VgmytpEx/exec";
